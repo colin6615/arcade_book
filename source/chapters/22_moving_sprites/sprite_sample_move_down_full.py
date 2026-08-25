1865baa92d3e0da3f2ee143ff3c3e763ad66d88c
colin <blooptroop333@gmail.com>
2026-08-25 13:36:48 -0700

    Rename on_update to update and pass delta_time

--- source/chapters/22_moving_sprites/sprite_sample_move_down_full.py
+++ source/chapters/22_moving_sprites/sprite_sample_move_down_full.py
@@ -25,7 +25,7 @@ class Coin(arcade.Sprite):
                                          SCREEN_HEIGHT + 100)
         self.center_x = random.randrange(SCREEN_WIDTH)
 
-    def on_update(self):
+    def update(self, delta_time):
 
         # Move the coin
         self.center_y -= 1
@@ -110,7 +110,7 @@ class MyGame(arcade.Window):
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
-        self.coin_list.update()
+        self.coin_list.update(delta_time)
 
         # Generate a list of all sprites that collided with the player.
         hit_list = arcade.check_for_collision_with_list(self.player_sprite,
