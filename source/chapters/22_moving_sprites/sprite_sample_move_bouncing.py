319e5ff9354fe4135c7f460481544e6514d66c9d
colin <blooptroop333@gmail.com>
2026-08-25 13:33:22 -0700

    Rename on_update method to update

--- source/chapters/22_moving_sprites/sprite_sample_move_bouncing.py
+++ source/chapters/22_moving_sprites/sprite_sample_move_bouncing.py
@@ -21,7 +21,7 @@ class Coin(arcade.Sprite):
         self.change_x = 0
         self.change_y = 0
 
-    def on_update(self, delta_time: float = 1 / 60) -> None:
+    def update(self, delta_time: float = 1 / 60) -> None:
 
         # Move the coin
         self.center_x += self.change_x
@@ -117,7 +117,7 @@ class MyGame(arcade.Window):
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
-        self.coin_list.on_update(delta_time)
+        self.coin_list.update(delta_time)
 
         # Generate a list of all sprites that collided with the player.
         hit_list = arcade.check_for_collision_with_list(self.player_sprite,
